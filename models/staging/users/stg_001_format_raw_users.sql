d142eb006402419b9194d1be3113ac2ad169bfdb
Xavier Carter <carterrxavier@gmail.com>
2026-04-12 12:53:19 -0500

    parse user jason

---
+++ models/staging/users/stg_001_format_raw_users.sql
@@ -0,0 +1,29 @@
+with src as (
+    select raw as j
+    from {{ source('raw_transactions_data', 'RAW_USERS') }}
+)
+
+select
+    j:user_id::number as user_id,
+    j:full_name::string as full_name,
+    j:email::string as email,
+    j:phone::string as phone,
+    j:device_id::string as device_id,
+
+    j:segment::string as segment,
+    j:avg_spending::number(38, 2) as avg_spending,
+
+    j:address_line1::string as address_line1,
+    j:address_line2::string as address_line2,
+    j:city::string as city,
+    j:state::string as state,
+    j:postal_code::string as postal_code,
+    j:country::string as country,
+
+    j:home_lat::float as home_lat,
+    j:home_lon::float as home_lon,
+
+    try_to_date(j:customer_since::string) as customer_since,
+    try_to_date(j:date_of_birth::string) as date_of_birth
+
+from src
